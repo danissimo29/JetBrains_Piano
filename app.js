4d5fc2bb9bc67b8352a56a8839eefdc935b1773f
danissimo29 <68133677+danissimo29@users.noreply.github.com>
2020-07-11 02:48:16 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,107 @@
+
+document.addEventListener("keydown", function (event) {
+    if (event.code == "KeyA") {
+        let audioA = new Audio("audio/A.mp3");
+        console.log("The 'A' key is pressed");
+        console.log(audioA.play());
+    } else if (event.code == "KeyS") {
+        let audioS = new Audio("audio/S.mp3");
+        console.log("The 'S' key is pressed");
+        console.log(audioS.play());
+    } else if (event.code == "KeyD") {
+        let audioD = new Audio("audio/D.mp3");
+        console.log("The 'D' key is pressed");
+        console.log(audioD.play());
+    } else if (event.code == "KeyF") {
+        let audioF = new Audio("audio/F.mp3");
+        console.log("The 'F' key is pressed");
+        console.log(audioF.play());
+    } else if (event.code == "KeyG") {
+        let audioG = new Audio("audio/G.mp3");
+        console.log("The 'G' key is pressed");
+        console.log(audioG.play());
+    } else if (event.code == "KeyH") {
+        let audioH = new Audio("audio/H.mp3");
+        console.log("The 'H' key is pressed");
+        console.log(audioH.play());
+    } else if (event.code == "KeyJ") {
+        let audioJ = new Audio("audio/J.mp3");
+        console.log("The 'J' key is pressed");
+        console.log(audioJ.play());
+    } else if (event.code == "KeyW") {
+        let audioW = new Audio("audio/W.mp3");
+        console.log("The 'W' key is pressed");
+        console.log(audioW.play());
+    } else if (event.code == "KeyE") {
+        let audioE = new Audio("audio/E.mp3");
+        console.log("The 'E' key is pressed");
+        console.log(audioE.play());
+    } else if (event.code == "KeyT") {
+        let audioT = new Audio("audio/T.mp3");
+        console.log("The 'T' key is pressed");
+        console.log(audioT.play());
+    } else if (event.code == "KeyY") {
+        let audioY = new Audio("audio/Y.mp3");
+        console.log("The 'Y' key is pressed");
+        console.log(audioY.play());
+    } else if (event.code == "KeyU") {
+        let audioU = new Audio("audio/U.mp3");
+        console.log("The 'U' key is pressed");
+        console.log(audioU.play());
+    }
+    else {
+        console.log("Unbound key is pressed");
+    }
+})
+
+
+
+/*document.addEventListener("keydown", function (event) {
+    let audioA = new Audio("audio/A.mp3");
+    if (event.code == "KeyA") {
+        console.log("The 'A' key is pressed");
+        console.log(audioA.play());
+    }
+})
+
+document.addEventListener("keydown", function (event) {
+    let audioS = new Audio("audio/S.mp3");
+    if (event.code == "KeyS") {
+        console.log("The 'S' key is pressed");
+        console.log(audioS.play());
+    }
+})
+
+document.addEventListener("keydown", function (event) {
+    let audioD = new Audio("audio/D.mp3");
+    if (event.code == "KeyD") {
+        console.log("The 'D' key is pressed");
+        console.log(audioD.play());
+    }
+})
+
+document.addEventListener("keydown", function (event) {
+    let audioF = new Audio("audio/F.mp3");
+    if (event.code == "KeyF") {
+        console.log("The 'F' key is pressed");
+        console.log(audioF.play());
+    }
+})
+
+document.addEventListener("keydown", function (event) {
+    let audioG = new Audio("audio/G.mp3");
+    if (event.code == "KeyG") {
+        console.log("The 'G' key is pressed");
+        console.log(audioG.play());
+    }
+})
+
+document.addEventListener("keydown", function (event) {
+    let audioJ = new Audio("audio/J.mp3");
+    if (event.code == "KeyJ") {
+        console.log("The 'J' key is pressed");
+        console.log(audioJ.play());
+    }
+    })
+
+*/
